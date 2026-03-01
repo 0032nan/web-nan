86e4e48d40e569f54af3543a023dfd933070949b
0032nan <0032nan@users.noreply.github.com>
2026-03-01 19:17:18 +0800

    Debug: Simplify to basic React app to test rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 const rootElement = document.getElementById('root')
 
-if (!rootElement) {
-  console.error('Root element not found!')
-  document.body.innerHTML = '<div style="color:red;padding:20px;">Error: Root element not found</div>'
-} else {
-  try {
-    createRoot(rootElement).render(
-      <StrictMode>
-        <App />
-      </StrictMode>,
-    )
-    console.log('React app mounted successfully')
-  } catch (error) {
-    console.error('Failed to mount React app:', error)
-    rootElement.innerHTML = `<div style="color:red;padding:20px;">
-      <h1>Error mounting app</h1>
-      <pre>${error.message}</pre>
-      <pre>${error.stack}</pre>
-    </div>`
-  }
+if (rootElement) {
+  createRoot(rootElement).render(<App />)
 }
